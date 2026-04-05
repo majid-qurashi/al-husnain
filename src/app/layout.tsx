d9ff3452b9f28575384e4460927b5966d5413129
aamirmoosa <gcet.aamir@gmail.com>
2026-04-05 13:02:03 +0530

    theme updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
           <CartProvider>
             <Header />
             <main className="flex-1">{children}</main>
